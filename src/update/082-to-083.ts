4d82817760e773f8c33fb4dd469b4a6dd898abb7
mswgen <54498184+mswgen@users.noreply.github.com>
2022-09-01 23:34:07 +0900

    update path

--- src/update/082-to-083.ts
+++ src/update/082-to-083.ts
@@ -49,7 +49,7 @@ export default {
             Intel Bluetooth: Add IntelBTPatcher.kext
         */
         if (plistParsed.Kernel.Add.some((x: any) => x.BundlePath == 'IntelBluetoothFirmware.kext') && !plistParsed.Kernel.Add.some((x: any) => x.BundlePath == 'IntelBTPatcher.kext')) {
-            cp.execSync(`cp -r "${os.homedir()}/.oc-update/${PID}/IntelBluetoothFirmware/IntelBTPatcher.kext" "${path.dirname(file)}/OC/Kexts"`);
+            cp.execSync(`cp -r "${os.homedir()}/.oc-update/${PID}/IntelBluetoothFirmware/IntelBTPatcher.kext" "${path.dirname(file)}/Kexts"`);
             plistParsed.Kernel.Add.push({
                 Arch: 'any',
                 BundlePath: 'IntelBTPatcher.kext',
