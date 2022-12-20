12b28b3f8146061047d2dcd54e9c63eb0d4e41be
mswgen <54498184+mswgen@users.noreply.github.com>
2022-12-20 23:49:59 +0900

    Create Project

---
+++ oc-updater/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  oc-updater
+//
+//  Created by mswgen on 2022/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("OpenCore Updater")
+                .font(.title)
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ oc-updater/oc_updaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  oc_updaterApp.swift
+//  oc-updater
+//
+//  Created by mswgen on 2022/12/20.
+//
+
+import SwiftUI
+
+@main
+struct oc_updaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
