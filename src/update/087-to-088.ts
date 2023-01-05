9373b41ddb85bfd417e7bd760c5ee4f0635370a0
mswgen <54498184+mswgen@users.noreply.github.com>
2023-01-05 13:23:44 +0900

    OC 0.8.8: create .contentVisibility

--- src/update/087-to-088.ts
+++ src/update/087-to-088.ts
@@ -1,4 +1,13 @@
+// import path and fs
+import fs from 'fs';
+import path from 'path';
 export default {
     from: 87,
-    configPlistChange: false
+    configPlistChange: true, // no change actually, only for .contentVisibility file
+    exec: (file: string) => {
+        // Create ../BOOT/.contentVisibility with content "Disabled"
+        // same with .contentVisibility
+        fs.writeFileSync(path.join(path.dirname(file), '..', 'BOOT', '.contentVisibility'), 'Disabled');
+        fs.writeFileSync(path.join(path.dirname(file), '.contentVisibility'), 'Disabled');
+    }
 }
